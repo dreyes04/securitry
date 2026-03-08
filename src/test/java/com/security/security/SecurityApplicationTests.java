f665ee170ad2f01a9b74fd77c8c953b50ca68ff2
dreyes04 <dreyesnegrete@gmail.com>
2026-03-07 18:56:32 -0600

    issues

--- src/test/java/com/security/security/SecurityApplicationTests.java
+++ src/test/java/com/security/security/SecurityApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This test verifies that the Spring Boot application context loads successfully.
+        // If the context fails to start, this test will automatically fail.
+    }
 
 }
